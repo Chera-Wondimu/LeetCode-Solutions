a67d1c34d7790252b8791fcbe475598375105fc3
Chera-Wondimu <chera.wondimu17@gmail.com>
2026-06-28 06:45:48 +0300

    Time: 2 ms (54.07%), Space: 19.4 MB (28.04%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -4,16 +4,13 @@ class Solution:
         count = Counter(s)
         length = 0
         odd = False
-
         for freq in count.values():
             if freq % 2 == 0:
                 length += freq
             else:
                 length += freq - 1
                 odd = True
-
         if odd:
             length += 1
-
         return length
         
