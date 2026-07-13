5b32921956c9601d9a3ad8cbb2fefc4b56268d73
Chera-Wondimu <chera.wondimu17@gmail.com>
2026-07-13 22:40:40 +0300

    Time: 7 ms (77.95%), Space: 21.2 MB (35.85%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        minutes = []
+        for time in timePoints:
+            h, m = map(int, time.split(":"))
+            minutes.append(h * 60 + m)
+        minutes.sort()
+        ans = float("inf")
+        for i in range(1, len(minutes)):
+            ans = min(ans, minutes[i] - minutes[i - 1])
+        ans = min(ans, 1440 - minutes[-1] + minutes[0])
+        return ans
