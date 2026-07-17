1a67e6ece3ea30a86772ee342d71a1c17cebaff3
Chera-Wondimu <chera.wondimu17@gmail.com>
2026-07-17 07:36:48 +0300

    Time: 0 ms (100%), Space: 21.1 MB (49.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,4 @@
-from collections import Counter
-
 class Solution:
-    def majorityElement(self, nums):
-        count = Counter(nums)
-        for num in count:
-            if count[num] > len(nums) // 2:
-                return num
+    def majorityElement(self, nums: List[int]) -> int:
+      nums.sort()
+      return nums[len(nums)//2]
