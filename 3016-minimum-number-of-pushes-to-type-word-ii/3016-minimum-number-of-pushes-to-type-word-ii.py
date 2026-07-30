a881232f3de8f5ba9231fee135c2a4a47e951909
Chera-Wondimu <chera.wondimu17@gmail.com>
2026-07-30 13:24:53 +0300

    Time: 183 ms (32.77%), Space: 19.9 MB (93.58%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,8 +1,11 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        freq = sorted(Counter(word).values(), reverse=True)
+        freq = {}
+        for ch in word:
+            freq[ch] = freq.get(ch, 0) + 1
+        arr = sorted(freq.values(), reverse=True)
         ans = 0
-        for i, f in enumerate(freq):
+        for i, f in enumerate(arr):
             ans += f * (i // 8 + 1)
         return ans
 
