1781e7ff327de442e677cbe954b820d0c38473f2
Chera-Wondimu <chera.wondimu17@gmail.com>
2026-08-19 05:33:57 +0300

    Time: 15 ms (18.66%), Space: 21.1 MB (83.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-      nums.sort()
-      return nums[len(nums)//2]
+        freq = {}
+        highest_count = 0
+        most_frequent = None
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+            if freq[num] > highest_count:
+                highest_count = freq[num]
+                most_frequent = num
+        return(most_frequent)
