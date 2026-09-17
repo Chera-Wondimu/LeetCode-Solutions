33e0c696b95f931b238fd12cb029a62f2a89b7ab
Chera-Wondimu <chera.wondimu17@gmail.com>
2026-09-17 07:24:30 +0300

    Time: 208 ms (78.32%), Space: 23.8 MB (67.75%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,19 @@
+class Solution:
+    def shipWithinDays(self, weights: list[int], days: int) -> int:
+        left = max(weights)
+        right = sum(weights)
+        while left < right:
+            mid = (left + right) // 2
+            current = 0
+            days_needed = 1
+            for weight in weights:
+                if current + weight > mid:
+                    days_needed += 1
+                    current = 0
+                current += weight
+            if days_needed > days:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+        
